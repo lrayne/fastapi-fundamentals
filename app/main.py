ccd9a1c59e136d6cca981a44ac445fc7946ef500
Aleksei <torsukov@MacBook-Pro-Aleksei.local>
2023-09-10 07:05:53 -0700

    Add send_feedback() usig post method

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from .models.models import FeedBack
 
 app = FastAPI()
 
@@ -11,6 +12,10 @@ fake_users = {
     6: {"username": "mad_smith", "email": "mad@example.com"}
 }
 
+fake_feedback_lst = [
+    {"name": "Alice", "comments": "Great course! I'm learning a lot."}
+]
+
 @app.get("/users")
 def read_all_users(limit: int = 5):
     return dict(list(fake_users.items())[:limit])
@@ -20,3 +25,13 @@ def read_user(user_id: int):
     if user_id in fake_users:
         return fake_users[user_id]
     return {"error": "User not found"}
+
+@app.post("/feedback")
+async def send_feedback(feedback: FeedBack):
+    fake_feedback_lst.append({"name": feedback.name, "comments": feedback.message})
+    return f"Feedback received. Thank you, {feedback.name}!"
+
+
+@app.get("/comments")
+async def show_feedback():
+    return fake_feedback_lst
--- app/models/models.py
+++ app/models/models.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 class User(BaseModel):
     name: str
     id: int
+
+
+class FeedBack(BaseModel):
+    name: str
+    message: str
