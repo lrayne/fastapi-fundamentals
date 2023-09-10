b764e4dcaf79b67929f8a406da6230b2ea3f8f94
Aleksei <torsukov@MacBook-Pro-Aleksei.local>
2023-09-10 06:44:27 -0700

    Add read_all_users() with limit  query param

--- app/main.py
+++ app/main.py
@@ -4,8 +4,16 @@ app = FastAPI()
 
 fake_users = {
     1: {"username": "john_doe", "email": "john@example.com"},
-    2: {"username": "jane_smith", "email": "jane@example.com"},
+    2: {"username": "robert_smith", "email": "robert@example.com"},
+    3: {"username": "aleksei_smith", "email": "aleksei@example.com"},
+    4: {"username": "anna_smith", "email": "anna@example.com"},
+    5: {"username": "galina_smith", "email": "galina@example.com"},
+    6: {"username": "mad_smith", "email": "mad@example.com"}
 }
+
+@app.get("/users")
+def read_all_users(limit: int = 5):
+    return dict(list(fake_users.items())[:limit])
 
 @app.get("/users/{user_id}")
 def read_user(user_id: int):
